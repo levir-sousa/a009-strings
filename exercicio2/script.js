5d500ce80c7267e472d7398461312aa8aa6cec4c
levir-sousa <levysouza2014@gmail.com>
2022-07-25 22:48:31 -0300

    exercicio2

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,12 @@
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+
+console.log(`Minha string antes tinha ${minhaString.length} caracteres.`);
+
+//a)
+const novaString = minhaString.trim()
+
+//b)
+console.log(`Minha string depois teve ${novaString.length} caracteres.`);
+
+//c)
+console.log(`${novaString.replace("________","sticioso")}`);
