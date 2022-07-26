27c253e443c5be429576a4e7d67f57048c8f6f67
levir-sousa <levysouza2014@gmail.com>
2022-07-25 22:48:38 -0300

    exercicio3

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,15 @@
-//Crie a const para a frase aqui
+//a)
+/*const frase = "Jorge tem uma casa verde e com portão azul, com os dizeres: "BOAS VINDAS, mas não deixe o gato sair"*/
+
+//b)
+const corCasa = "rosa"
+const corPortao = "laranja"
+
+//EXTRA:
+const gato = "mas não deixe o gato sair"
+
+//a)
+const frase = `Jorge tem uma casa ${corCasa} e com portão ${corPortao}, com os dizeres: "BOAS VINDAS, ${gato.toUpperCase()}"`
+
+//c)
+console.log(frase);
